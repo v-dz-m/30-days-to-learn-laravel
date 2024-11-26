d6c6317f58a66952174b9dfa3cf27ba868c8881a
Vadzim Hanchuk <31338180+v-dz-m@users.noreply.github.com>
2024-11-26 16:59:39 +0300

    Add day no. 21, pt. 1

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
-Route::view('/contact', 'contact');
+Route::get('/register', [RegisteredUserController::class, 'create']);
