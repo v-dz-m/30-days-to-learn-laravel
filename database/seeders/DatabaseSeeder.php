740c3c342e8084e06d695faf91f4fdba45285e00
Vadzim Hanchuk <31338180+v-dz-m@users.noreply.github.com>
2024-11-26 10:46:11 +0300

    Add days no. 15 and 16

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
